06775c35f2fc911d449e676f95149d9a8a7700e3
srinivas486 <srinivas486@users.noreply.github.com>
2026-03-17 00:46:24 +0000

    deploy: srinivas486/skillfield-landing@05fc5b76cd9e1b855ce300d0aff263ad3e688189

---
+++ _next/static/chunks/app/layout-e1494cb7a12a549b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{2221:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4546,23))},4546:()=>{}},e=>{e.O(0,[289,441,255,358],()=>e(e.s=2221)),_N_E=e.O()}]);
